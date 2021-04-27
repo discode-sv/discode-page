77cef7677dc4102042d9b9e4d14148c42b8fe952
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-04-27 01:43:57 -0300

    Limpieza general

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Readme from '../../README.md';
-import { Container } from '../components/Container';
-import { Site } from '../components/Site';
 
 const seo = {
   title: 'Home',
 };
 
-const IndexPage = (props) => (
-  <Site seo={seo} {...props}>
-    <Container>
-      <Readme />
-    </Container>
-  </Site>
-);
-
-export default IndexPage;
+const Index = (props) => {
+  return(
+    <>
+      Soy una pagina en blanco
+    </>
+  )
+}
+export default Index;
